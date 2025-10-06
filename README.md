2c2bab8e88b120ff054eaca0341b33460056593b
jakob <138621739+jakobsereda@users.noreply.github.com>
2025-10-05 21:44:40 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Gander Programming Language
+
+Goals:
+- primitive types
+- arithmetic and logic
+- conditional logic
+- functions
+- enums and structs
+- hindley-miller type inference
+- polymorphism
+- closures/lexical scope
+- pattern matching
---
+++ bin/src/main.rs
@@ -0,0 +1,24 @@
+use std::fs;
+use clap::Parser;
+
+/// A compiler for the Gander programming language
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Path to .gand source file
+    #[arg(short, long)]
+    path: String,
+
+    /// Turns debugging info on
+    #[arg(short, long)]
+    debug: bool
+}
+
+fn main() {
+    let args = Args::parse();
+
+    let data = fs::read_to_string(args.path)
+        .expect("Unable to read from provided filepath");
+
+    println!("{}", data);
+}
