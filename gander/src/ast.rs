10b4e220aaaaada6cc480c7d2a1001aa2780b6ab
jakob <138621739+jakobsereda@users.noreply.github.com>
2025-11-26 21:33:11 -0800

    feat: finished ast (for now?)

--- gander/src/ast.rs
+++ gander/src/ast.rs
@@ -88,3 +88,95 @@ pub struct FunctionType {
     pub params: Vec<Type>,
     pub ret: Box<Type>,
 }
+
+#[derive(Debug)]
+pub enum Expression {
+    Comparison {
+        lhs: Box<Expression>,
+        op: Option<ComparisonOp>,
+        rhs: Option<Box<Expression>>,
+    },
+
+    Additive {
+        lhs: Box<Expression>,
+        op: Option<AdditiveOp>,
+        rhs: Option<Box<Expression>>,
+    },
+
+    Multiplicative {
+        lhs: Box<Expression>,
+        op: Option<MultiplicativeOp>,
+        rhs: Option<Box<Expression>>,
+    },
+
+    Unary {
+        op: Option<UnaryOp>,
+        body: Box<Expression>,
+    },
+
+    Primary(PrimaryExpr),
+}
+
+#[derive(Debug)]
+pub enum ComparisonOp {
+    Equals,
+    NotEquals,
+    GreaterThan,
+    LessThan,
+    GreaterEquals,
+    LessEquals,
+}
+
+#[derive(Debug)]
+pub enum AdditiveOp {
+    Plus,
+    Minus,
+}
+
+#[derive(Debug)]
+pub enum MultiplicativeOp {
+    Divide,
+    Multiply,
+    Modulo,
+}
+
+#[derive(Debug)]
+pub enum UnaryOp {
+    Negative,
+}
+
+#[derive(Debug)]
+pub enum PrimaryExpr {
+    StructAccess(StructAccess),
+    EnumAccess(EnumAccess),
+    FunctionCall(FunctionCall),
+    Identifier(String),
+    Literal(Literal),
+    ParenExpr(Box<Expression>),
+}
+
+#[derive(Debug)]
+pub struct StructAccess {
+    pub parent: Box<Expression>,
+    pub field: String,
+}
+
+#[derive(Debug)]
+pub struct EnumAccess {
+    pub parent: String,
+    pub variant: String,
+}
+
+#[derive(Debug)]
+pub struct FunctionCall {
+    pub ident: String,
+    pub args: Vec<Expression>,
+}
+
+#[derive(Debug)]
+pub enum Literal {
+    Int(i64),
+    Float(f64),
+    Bool(bool),
+    String(String),
+}
