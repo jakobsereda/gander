612073285c78d98623b7b0d1b0df3b03266f5a59
jakob <138621739+jakobsereda@users.noreply.github.com>
2025-11-26 21:50:46 -0800

    feat: public api for parser
    some wrappers around tokenizer methods too

--- gander/src/parser.rs
+++ gander/src/parser.rs
@@ -1,8 +1,42 @@
-use crate::tokenizer::Tokenizer;
+use std::iter::Peekable;
+
+use crate::{
+    ast::*,
+    token::Token,
+    tokenizer::Tokenizer
+};
 
 #[derive(Debug)]
 struct Parser<'a> {
-    tokenizer: Tokenizer<'a>,
+    tokenizer: Peekable<Tokenizer<'a>>,
 }
 
+impl<'a> Parser<'a> {
+    pub fn new(src: &'a str) -> Self {
+        Self {
+            tokenizer: Tokenizer::new(src).peekable(),
+        }
+    }
 
+    pub fn parse_program(&mut self) -> Program {
+        let mut items = Vec::new();
+
+        while let Some(_) = self.peek() {
+            items.push(self.parse_item());
+        }
+        
+        Program { items }
+    }
+
+    fn parse_item(&mut self) -> Item {
+        Item::Expression(Expression::Primary(PrimaryExpr::Literal(Literal::Int(0))))
+    }
+
+    fn peek(&mut self) -> Option<&Token> {
+        self.tokenizer.peek()
+    }
+
+    fn eat(&mut self) -> Option<Token> {
+        self.tokenizer.next()
+    }
+}
