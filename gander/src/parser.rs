d121e4f40b638486970dddd25f25c9cbe0fab2d9
jakobsereda <jakobsereda@gmail.com>
2025-12-06 16:41:39 -0800

    feat: added helpers to parser

--- gander/src/parser.rs
+++ gander/src/parser.rs
@@ -1,6 +1,9 @@
 use crate::{
     ast::*,
-    token::{TokenVariant, Symbol},
+    token::{
+        TokenVariant,
+        Symbol
+    },
     tokenbuffer::TokenBuffer,
     tokenizer::Tokenizer
 };
@@ -23,7 +26,6 @@ impl<'a> Parser<'a> {
     // Entry point for the parser
     pub fn parse_program(&mut self) -> Program {
         let mut items = Vec::new();
-
         while self.tokens.peek().is_some() {
             items.push(self.parse_item());
         }
@@ -34,22 +36,21 @@ impl<'a> Parser<'a> {
     // EBNF:
     //     item = definition
     //          | expression;
+    // TODO: this function is ugly
     fn parse_item(&mut self) -> Item {
-        let equals = TokenVariant::Symbol(Symbol::Equals);
-        let at = TokenVariant::Symbol(Symbol::At);
 
         // A variable definition can either have an
         // exlicit type or not. i.e.
         //     Int a = 2
         // or
         //     a = 2
-        if self.check(equals, 1) || self.check(equals, 2) {
+        if self.check(TokenVariant::Symbol(Symbol::Equals), 1) || self.check(TokenVariant::Symbol(Symbol::Equals), 2) {
             Item::Definition(self.parse_definition())
-        } else if self.check(at, 0) {
+        } else if self.check(TokenVariant::Symbol(Symbol::At), 0) {
             let sig = self.parse_function_type_decl();
             Item::Definition(Definition::Function(
                 FunctionDef {
-                    sig: Some(sig), 
+                    sig: Some(sig),
                     head: self.parse_function_header(),
                 }
             ))
@@ -64,18 +65,19 @@ impl<'a> Parser<'a> {
     //                | variable_def
     //                | function_def;
     fn parse_definition(&mut self) -> Definition {
-        match self.tokens.peek_n(2) {
+        let t = self.tokens.peek_n(2).unwrap();
+        match t.var {
             TokenVariant::Symbol(Symbol::Hash) => {
                 Definition::Enum(self.parse_enum_def())
             },
             TokenVariant::Symbol(Symbol::Dollar) => {
-                Defintion::Struct(self.parse_struct_def())
+                Definition::Struct(self.parse_struct_def())
             },
             TokenVariant::Symbol(Symbol::Colon) => {
-                Defintion::Function(self.parse_function_def())
+                Definition::Function(self.parse_function_def())
             },
             _ => {
-                // At this point, I think we can assert that 
+                // At this point, I think we can assert that
                 // the 2nd token in the buffer is Symbol::Equals,
                 // So it has to be variable definition
                 Definition::Variable(self.parse_variable_def())
@@ -90,7 +92,11 @@ impl<'a> Parser<'a> {
     // TODO: ebnf dosen't have commas for enums
     // and structs
     fn parse_enum_def(&mut self) -> EnumDef {
-        let ident = self.tokens.eat().unwrap();
+        let ident = self.tokens
+            .eat()
+            .unwrap()
+            .lit
+            .unwrap();
 
         // Consume Symbol::Equals
         self.tokens.eat();
@@ -99,26 +105,23 @@ impl<'a> Parser<'a> {
         self.tokens.eat();
 
         let mut variants = Vec::new();
-        while self.check(TokenVariant::Symbol(Symbol::Comma), 1) {
+        loop {
             let var_id = self.tokens.eat().unwrap();
             variants.push(var_id.lit.unwrap());
+
+            if self.check(TokenVariant::Symbol(Symbol::Comma), 0) {
+                break;
+            }
 
             // Consume Symbol::Comma
             self.tokens.eat();
         }
-
-        // There will be one variant remaining after
-        // we have consumed all of the ones with a 
-        // trailing comma.
 
         // TODO: maybe we don't want it to be 
         // mandatory that there is no comma after the
         // last variant, having an optional comma
         // could be nice. But then I would have to 
         // implement the whitespace tokens ughhhhhh
-
-        let var_id = self.tokens.eat().unwrap();
-        variants.push(var_id.lit.unwrap());
 
         EnumDef {
             ident,
@@ -128,7 +131,13 @@ impl<'a> Parser<'a> {
 
     // TODO: similar, error handling
     fn parse_struct_def(&mut self) -> StructDef {
-        let ident = self.tokens.eat().unwrap();
+        // TODO: this pattern (extracting a list) is used
+        // a lot, maybe it could become a helper function
+        let ident = self.tokens
+            .eat()
+            .unwrap()
+            .lit
+            .unwrap();
 
         // Consume Symbol::Equals
         self.tokens.eat();
@@ -137,18 +146,16 @@ impl<'a> Parser<'a> {
         self.tokens.eat();
 
         let mut fields = Vec::new();
-        while self.check(TokenVariant::Symbol(Symbol::Comma), 3) {
+        loop {
             fields.push(self.parse_struct_field());
+
+            if !self.check(TokenVariant::Symbol(Symbol::Comma), 0) {
+                break;
+            }
 
             // Consume Symbol::Comma
             self.tokens.eat();
         }
-
-        // Same issue as in parse_enum_def, this
-        // requires no trailing comma on the last
-        // field, which is less than ideal
-
-        fields.push(self.parse_struct_field());
 
         StructDef {
             ident,
@@ -157,7 +164,11 @@ impl<'a> Parser<'a> {
     }
 
     fn parse_struct_field(&mut self) -> StructField {
-        let ident = self.tokens.eat().unwrap();
+        let ident = self.tokens
+            .eat()
+            .unwrap()
+            .lit
+            .unwrap();
 
         // Consume Symbol::Colon
         self.tokens.eat();
@@ -171,12 +182,12 @@ impl<'a> Parser<'a> {
     }
 
     fn parse_variable_def(&mut self) -> VariableDef {
-        let vtype: Option<Type> = None;
+        let mut vtype: Option<Type> = None;
         if self.check(TokenVariant::Symbol(Symbol::Equals), 2) {
             vtype = Some(self.parse_type());
         }
 
-        let ident = self.tokens.eat().unwrap();
+        let ident = self.eat_and_extract_lit();
 
         // Consume Symbol::Equals
         self.tokens.eat();
@@ -191,7 +202,7 @@ impl<'a> Parser<'a> {
     }
 
     fn parse_function_def(&mut self) -> FunctionDef {
-        let sig: Option<FunctionTypeDecl> = None;
+        let mut sig: Option<FunctionTypeDecl> = None;
         if self.check(TokenVariant::Symbol(Symbol::At), 0) {
             sig = Some(self.parse_function_type_decl());
         }
@@ -208,20 +219,17 @@ impl<'a> Parser<'a> {
         // Consume Symbol::At
         self.tokens.eat();
 
-        let params = Vec::new();
-        while !self.check(TokenVariant::Symbol(Symbol::Arrow), 1) {
+        let mut params = Vec::new();
+        loop {
             params.push(self.parse_type());
 
-            // Consume Symbol::Comma
-            self.tokens.eat();
+            // Consume Symbol::Comma or Symbol::Arrow
+            if self.eat_and_match(TokenVariant::Symbol(Symbol::Arrow)) {
+                break;
+            }
         }
 
-        params.push(self.parse_type());
-
-        // Consume Symbol::Arrow
-        self.tokens.eat();
-
-        let ret = self.tokens.eat();
+        let ret = self.parse_type();
 
         FunctionTypeDecl {
             params,
@@ -230,14 +238,44 @@ impl<'a> Parser<'a> {
     }
 
     fn parse_function_header(&mut self) -> FunctionHeader {
-        todo!()
+        let ident = self.eat_and_extract_lit();
+
+        // Consume Symbol::Equals
+        self.tokens.eat();
+
+        // Consume Symbol::Colon
+        self.tokens.eat();
+
+        // Consume Symbol::LParen
+        self.tokens.eat();
+
+        let mut params = Vec::new();
+        loop {
+            let token = self.tokens.eat().unwrap();
+            params.push(token.lit.unwrap());
+
+            // Consume Symbol::Comma or Symbol::RParen
+            if self.eat_and_match(TokenVariant::Symbol(Symbol::RParen)) {
+                break;
+            }
+        }
+
+        let body = Vec::new();
+
+        FunctionHeader {
+            ident,
+            params,
+            body
+        }
     }
 
     fn parse_type(&mut self) -> Type {
-        match self.tokens.peek() {
+        let t = self.tokens.peek().unwrap();
+
+        match t.var {
             TokenVariant::Identifier => {
-                let id = self.tokens.eat().unwrap();
-                Type::Identifier(id.lit.unwrap())
+                let id = self.eat_and_extract_lit();
+                Type::Identifier(id)
             },
             TokenVariant::Symbol(Symbol::LParen) => {
                 Type::Function(self.parse_function_type())
@@ -251,7 +289,7 @@ impl<'a> Parser<'a> {
     fn parse_primitive_type(&mut self) -> PrimitiveType {
         let t = self.tokens.eat().unwrap();
 
-        match t {
+        match t.var {
             TokenVariant::Symbol(Symbol::IntType) => PrimitiveType::Int,
             TokenVariant::Symbol(Symbol::BoolType) => PrimitiveType::Bool,
             TokenVariant::Symbol(Symbol::FloatType) => PrimitiveType::Float,
@@ -264,26 +302,24 @@ impl<'a> Parser<'a> {
         // Consume Symbol::LParen
         self.tokens.eat();
 
-        let params = Vec::new();
-        while !self.check(TokenVariant::Symbol(Symbol::RParen), 1) {
+        let mut params = Vec::new();
+        loop {
             params.push(self.parse_type());
 
-            // Consume Symbol::Comma
-            self.tokens.eat();
+            // Consume Symbol::Comma or Symbol::RParen
+            if self.eat_and_match(TokenVariant::Symbol(Symbol::RParen)) {
+                break;
+            }
         }
-
-        // Should be one last param
-        params.push(self.parse_type());
-
-        // Consume Symbol::RParen
-        self.tokens.eat();
 
         // Consume Symbol::Arrow
         self.tokens.eat();
 
+        let ret = Box::new(self.parse_type());
+
         FunctionType {
             params,
-            Box::new(self.parse_type())
+            ret
         }
     }
 
@@ -298,12 +334,17 @@ impl<'a> Parser<'a> {
         }
     }
 
-    fn eat_if_match(&mut self, expected: TokenVariant) -> bool {
-        if self.check(expected, 0) {
-            self.tokens.eat();
-            true
-        } else {
-            false
-        }
+    fn eat_and_match(&mut self, expected: TokenVariant) -> bool {
+        let matches = self.check(expected, 0);
+        self.tokens.eat();
+        matches
+    }
+
+    fn eat_and_extract_lit(&mut self) -> String {
+        self.tokens
+            .eat()
+            .expect("eat_and_extract_lit: expected Token, got None")
+            .lit
+            .expect("eat_and_extract_lit: should not be invoked when Token has no lit")
     }
 }
